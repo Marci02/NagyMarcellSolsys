10bcf058e2ba61fea81a845d0f0185dbe6fba7fc
Marci02 <nagymarci.0218@gmail.com>
2023-10-25 09:42:05 +0200

    Add project files.

---
+++ doga/Bolygo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doga
+{
+    internal class Bolygo
+    {
+        public string Nev { get; set; }
+        public int HoldSzama { get; set; }
+        public double Aranya { get; set; }
+
+        public Bolygo(string v)
+        {
+            string[] adatok = v.Split(';');
+            Nev = adatok[0];
+            HoldSzama = int.Parse(adatok[1]);
+            Aranya = double.Parse(adatok[2].Replace('.' , ','));
+        }
+    }
+}
---
+++ doga/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+namespace doga
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var bolygok = new List<Bolygo>();
+            using (var sr = new StreamReader(@"..\..\..\src\solsys.txt"))
+            {
+                while (!sr.EndOfStream) bolygok.Add(new Bolygo(sr.ReadLine()));
+            }
+
+            Console.WriteLine("3: feladat:");
+
+            Console.WriteLine($"3.1: {bolygok.Count} bolygó van a naprendszerben");
+
+            Console.WriteLine($"3.2: a naprendszerben egy bolygónak átlagosan {bolygok.Average(b => b.HoldSzama)}");
+
+            var maxbolygo = bolygok.SingleOrDefault(b => b.Aranya == bolygok.Max(b2 => b2.Aranya));
+            Console.WriteLine($"3.3: a legnagyobb térfogatú bolygó a" +
+                $" {maxbolygo.Nev}");
+
+            Console.Write("3.4: Írd be a keresett bolygó nevét: ");
+            var sor = Console.ReadLine();
+            Console.WriteLine(bolygok.SingleOrDefault(b => b.Nev == sor) != null ?
+                "Van ilyen bolgyó a naprendszerben." :
+                "Sajnos nincsen ilyen bolygó a naprendszerben.");
+
+            Console.Write("3.5: Írj be egy egész számot: ");
+            var szam = int.Parse(Console.ReadLine());
+            var tobbHold = bolygok.Where(b => b.HoldSzama > szam).Select(b => b.Nev).ToArray();
+            Console.WriteLine($"a következő bolygóknak van {szam}-nál/nél több holdja:");
+            foreach (var item in tobbHold)
+            {
+                Console.Write($"{item}, ");
+            }
+
+
+        } 
+    }
+}
